56cf592bd55ce73815a774014108fd478baf3a44
0xNotes <notes@sigil.systems>
2022-11-03 22:27:09 -0400

    Added more assets

--- src/pages/premint.tsx
+++ src/pages/premint.tsx
@@ -1,15 +1,21 @@
 import TextContainer from "../components/textContainer"
 import logo from "../assets/logo.gif";
+import dna from "../assets/dna.gif";
+import pc from "../assets/pc.gif";
+import scientist from "../assets/scientist.gif";
+import wings from "../assets/wings.gif";
 
 export default function Home() {
     return(
         <div className="Container">
             <img src={logo} style={{marginTop: "4rem"}}></img>
+            <div>
             <TextContainer
             topText="TESTTOP"
             bottomText="TESTBOT"
             reversed={false}
             />
+            </div>
         </div>
     );
 }
