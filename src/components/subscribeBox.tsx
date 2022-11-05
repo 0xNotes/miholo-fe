4192c9764c24e7d5ea86d90b0017ffa87e313de9
0xNotes <notes@sigil.systems>
2022-11-05 13:07:43 -0400

    Added subscribe form, need to hook it to logic

--- src/components/subscribeBox.tsx
+++ src/components/subscribeBox.tsx
@@ -1,9 +1,13 @@
 
 export default function SubscribeBox() {
-    return(
-        <div className="SubscribeBox">
-            SUBSCRIBEBOX
-        </div>
-
+    return (
+        <form className="SubscribeBox">
+            <div>
+                <input className="InputText" type="email" placeholder="your email..." name="name" />
+            </div>
+            <div>
+                <input className="SubmitButton" type="submit" value="Subscribe" />
+            </div>
+        </form>
     );
 }
