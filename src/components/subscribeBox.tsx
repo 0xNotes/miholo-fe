0b5053a3f2d422e7a1b5ded699d42308af141364
0xNotes <notes@sigil.systems>
2022-11-07 17:36:43 -0500

    Tested API

--- src/components/subscribeBox.tsx
+++ src/components/subscribeBox.tsx
@@ -1,3 +1,75 @@
+import axios from "axios";
+
+type EmailResponse ={
+    email: string;
+};
+
+type ShippingResponse ={
+    name: string;
+    address_line_1: string;
+    address_line_2: string;
+    city_state_zip: string;
+    country: string;
+}
+
+
+async function addEmail(inputEmail: string) {
+    try {
+      // 👇️ const data: CreateUserResponse
+      const { data } = await axios.post<EmailResponse>(
+        'http://127.0.0.1:5000/add_email',
+        { email: inputEmail},
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+        },
+      );
+  
+      console.log(JSON.stringify(data, null, 4));
+  
+      return data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.log('error message: ', error.message);
+        // 👇️ error: AxiosError<any, any>
+        return error.message;
+      } else {
+        console.log('unexpected error: ', error);
+        return 'An unexpected error occurred';
+      }
+    }
+  }
+
+async function addShipping(inputName: string, inputAddress1: string, inputAddress2: string, inputCityStateZip: string, inputCountry: string) {
+    try {
+      // 👇️ const data: CreateUserResponse
+      const { data } = await axios.post<ShippingResponse>(
+        'http://127.0.0.1:5000/add_shipping',
+        { name: inputName, address_line_1: inputAddress1, address_line_2: inputAddress2, city_state_zip: inputCityStateZip, country: inputCountry},
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+        },
+      );
+  
+      console.log(JSON.stringify(data, null, 4));
+  
+      return data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.log('error message: ', error.message);
+        // 👇️ error: AxiosError<any, any>
+        return error.message;
+      } else {
+        console.log('unexpected error: ', error);
+        return 'An unexpected error occurred';
+      }
+    }
+  }
 
 export default function SubscribeBox() {
     return (
@@ -7,6 +79,9 @@ export default function SubscribeBox() {
             </div>
             <div>
                 <input className="SubmitButton" type="submit" value="Subscribe" />
+                <button onClick={ () => addEmail("fromReact@gmail.com")}>Email</button>
+                <button onClick={ () => addShipping("john", "420 Blaze It Lane", "APPT: REAR", "Methton, Kholia, 69696", "USA")}>Ship</button>
+
             </div>
         </form>
     );
